d94d9f8963e75f685d8704121ff63037fd80884a
Søren Kottal <sk@ecreo.dk>
2022-01-12 19:06:01 +0100

    first

---
+++ src/skttl.Guid/Main.cs
@@ -0,0 +1,72 @@
+﻿using ManagedCommon;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Wox.Plugin;
+
+namespace skttl.Guid
+{
+    public class Main : IPlugin
+    {
+        private string IconPath { get; set; }
+
+        private PluginInitContext Context { get; set; }
+        public string Name => "Guid";
+
+        public string Description => "Guid Generator";
+
+        public List<Result> Query(Query query)
+        {
+            var guid = System.Guid.NewGuid().ToString();
+            var guidUpper = guid.ToUpperInvariant();
+
+            return new List<Result>
+            {
+                new Result
+                {
+                    Title = guid,
+                    IcoPath = IconPath,
+                    Action = e =>
+                    {
+                        Clipboard.SetText(guid);
+                        return true;
+                    },
+                },
+
+                new Result
+                {
+                    Title = guidUpper,
+                    IcoPath = IconPath,
+                    Action = e =>
+                    {
+                        Clipboard.SetText(guidUpper);
+                        return true;
+                    },
+                },
+            };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            Context = context;
+            Context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(Context.API.GetCurrentTheme());
+        }
+
+        private void UpdateIconPath(Theme theme)
+        {
+            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+            {
+                IconPath = "images/guid.light.png";
+            }
+            else
+            {
+                IconPath = "images/guid.dark.png";
+            }
+        }
+
+        private void OnThemeChanged(Theme currentTheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+    }
+}
